5a3b0ddb626b882882e2dbb4c1b6911b5c6bd6eb
many9568 <many9568@naver.com>
2023-11-10 12:09:50 +0900

    이름 안변경

--- app.py
+++ app.py
@@ -2,10 +2,17 @@
 # A very simple Flask Hello World app for you to get started with...
 
 from flask import Flask
+from collections import Counter
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '안녕 반가워 내이름은 안알랴줌'
+    user_string = "그래그래 나도나도 니도니도 코딩코딩 전부전부전부 코딩코딩 코딩 이다 이다"
+    counter = dict=(Counter(user_string0))
+    result = json.dumpy(counter)
+
+    return result
+
 
