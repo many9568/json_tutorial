ded11a4ff040d4e42d95c4abed34f2fa8240209d
many9568 <many9568@naver.com>
2023-11-10 12:11:22 +0900

    이름 안변경1

--- app.py
+++ app.py
@@ -10,9 +10,8 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     user_string = "그래그래 나도나도 니도니도 코딩코딩 전부전부전부 코딩코딩 코딩 이다 이다"
-    counter = dict=(Counter(user_string0))
+    counter = dict=(Counter(user_string))
     result = json.dumpy(counter)
-
     return result
 
 
